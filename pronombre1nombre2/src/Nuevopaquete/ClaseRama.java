7eaba7dbb64d097c95188df33d75ff66bb9aa452
Mauro <Mauro@DESKTOP-GR8A5PE>
2025-05-16 00:07:18 +0200

    Modificación en ClaseRama.java desde rama ramanombre1nombre2

---
+++ pronombre1nombre2/src/Nuevopaquete/ClaseRama.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Nuevopaquete;
+
+/**
+ *
+ * @author Mauro
+ */
+public class ClaseRama {
+    public static void mostrarMensaje() {
+        System.out.println("Esto es una clase nueva en la rama.");
+        System.out.println("Modificación hecha en la rama ramanombre1nombre2");
+    }
+}
