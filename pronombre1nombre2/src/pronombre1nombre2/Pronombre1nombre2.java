d505085e526929cc4e4a6bccecb5b503eff254a4
Mauro <Mauro@DESKTOP-GR8A5PE>
2025-05-15 11:56:12 +0200

    Primer commit: añadido mensaje en consola

---
+++ pronombre1nombre2/src/pronombre1nombre2/Pronombre1nombre2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pronombre1nombre2;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Pronombre1nombre2 {
+    public static void main(String[] args) {
+        System.out.println("Hola desde Git en NetBeans");
+    }
+}
